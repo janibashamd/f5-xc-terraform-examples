4cf79f399f116412cbd6727d96da1ef0db7d4358
Janibasha <m.janibasha@f5.com>
2024-10-01 09:05:03 +0530

    added subnet id

--- gcp/bigip/bigip.tf
+++ gcp/bigip/bigip.tf
@@ -6,6 +6,23 @@ module bigip {
   zone            = local.region
   image           = var.image
   service_account = local.service_account
-  #f5_password     = var.bigip_password
-  mgmt_subnet_ids = [{ "subnet_id" = local.subnet_id, "public_ip" = true, "private_ip_primary" = "" }]
+  f5_password     = var.bigip_password
+  vm_name         = "${local.project_prefix}-bigip"
+  network_tags    = "bigip"
+  mgmt_subnet_ids = [{ "subnet_id" = "projects/f5-gcs-5611-mktg-secsols/regions/asia-south1/subnetworks/jani-bd-subnet-ba9f", "public_ip" = true, "private_ip_primary" = "" }]
+}
+
+# firewall rules
+resource "google_compute_firewall" "allow-ports" {
+  name          = "${local.project_prefix}-allow"
+  network       = local.network_name
+  allow {
+    protocol    = "tcp"
+    ports       = ["22", "80", "443", "8443"]
+  }
+  allow {
+    protocol    = "icmp"
+  }
+  source_ranges = ["0.0.0.0/0"]
+  target_tags   = ["bigip"]
 }
--- gcp/bigip/variables.tf
+++ gcp/bigip/variables.tf
@@ -9,3 +9,9 @@ variable "image" {
   description = "BIGIP image name"
   default     = "projects/f5-7626-networks-public/global/images/f5-bigip-17-1-1-4-0-0-9-payg-good-1gbps-240902164747"
 }
+
+variable "bigip_password" {
+  type        = string
+  description = "BIGIP instance password"
+  default     = "Admin@12345"
+}
