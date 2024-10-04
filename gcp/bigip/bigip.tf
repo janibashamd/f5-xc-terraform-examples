1af65ceed0b036b94947682615ded157b54a906c
Janibasha <m.janibasha@f5.com>
2024-10-04 20:33:33 +0530

    Update bigip.tf

--- gcp/bigip/bigip.tf
+++ gcp/bigip/bigip.tf
@@ -1,16 +1,20 @@
 module bigip {
-  count           = 1
-  source          = "F5Networks/bigip-module/gcp"
-  prefix          = local.project_prefix
-  project_id      = local.project_id
-  zone            = "${local.region}-a"
-  image           = var.image
-  service_account = local.service_account
-  f5_password     = var.bigip_password
-  vm_name         = "${local.project_prefix}-bigip"
-  network_tags    = ["bigip"]
-  f5_ssh_publickey= "./id_rsa.pub"
-  mgmt_subnet_ids = [{ "subnet_id" = null, "public_ip" = null, "private_ip_primary" = null }, { "subnet_id" = local.subnet_id, "public_ip" = true, "private_ip_primary" = "" }]
+  count            = 1
+  source           = "F5Networks/bigip-module/gcp"
+  prefix           = local.project_prefix
+  project_id       = local.project_id
+  zone             = "${local.region}-a"
+  image            = var.image
+  service_account  = local.service_account
+  f5_password      = var.bigip_password
+  vm_name          = "${local.project_prefix}-bigip"
+  network_tags     = ["bigip"]
+  f5_ssh_publickey = "./id_rsa.pub"
+  mgmt_subnet_ids  = [
+    { "subnet_id" = null, "public_ip" = null, "private_ip_primary" = null },
+    { "subnet_id" = local.subnet_id, "public_ip" = true, "private_ip_primary" = "" }
+  ]
+}
 
 # firewall rules
 resource "google_compute_firewall" "allow-ports" {
