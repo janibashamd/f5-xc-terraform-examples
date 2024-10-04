5d5a6ab56eafc97265ad3d0eba4361cc179dc7bb
Janibasha <m.janibasha@f5.com>
2024-10-04 20:06:59 +0530

    debug5

--- gcp/bigip/locals.tf
+++ gcp/bigip/locals.tf
@@ -3,8 +3,8 @@ locals {
   region          = data.tfe_outputs.gcp-infra.values.gcp_region
   network_name    = data.tfe_outputs.gcp-infra.values.vpc_name
   subnet_name     = data.tfe_outputs.gcp-infra.values.vpc_subnet
-  #subnet_id       = data.tfe_outputs.gcp-infra.values.vpc_subnet_id
-  subnet_id       = "projects/f5-gcs-5611-mktg-secsols/regions/asia-south1/subnetworks/jani-bd-subnet-ba9f"
+  subnet_id       = data.tfe_outputs.gcp-infra.values.vpc_subnet_id
+  #subnet_id       = "projects/f5-gcs-5611-mktg-secsols/regions/asia-south1/subnetworks/jani-bd-subnet-ba9f"
   project_prefix  = data.tfe_outputs.gcp-infra.values.project_prefix
   service_account = data.tfe_outputs.gcp-infra.values.service_account
 }
