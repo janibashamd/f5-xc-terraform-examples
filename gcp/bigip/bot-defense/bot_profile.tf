0c4ff5f00174a6c7e80f0aa650c7caa11ee47e5d
Janibasha <m.janibasha@f5.com>
2024-10-10 15:14:29 +0530

    added pool code

--- gcp/bigip/bot-defense/bot_profile.tf
+++ gcp/bigip/bot-defense/bot_profile.tf
@@ -4,6 +4,44 @@ provider "bigip" {
     password = local.bigip_password
 }
 
+resource "bigip_ltm_monitor" "monitor" {
+  name   = "/Common/terraform_monitor"
+  parent = "/Common/http"
+}
+
+resource "bigip_ltm_node" "node" {
+  name             = "/Common/terraform_node1"
+  address          = local.app_ip
+  connection_limit = "0"
+  dynamic_ratio    = "1"
+  monitor          = "/Common/icmp"
+  description      = "Test-Node"
+  rate_limit       = "disabled"
+  fqdn {
+    address_family = "ipv4"
+    interval       = "3000"
+  }
+}
+#
+#resource "bigip_ltm_pool" "pool" {
+#  name                   = "/Common/APP1_Pool"
+#  load_balancing_mode    = "round-robin"
+#  minimum_active_members = 1
+#  monitors               = [bigip_ltm_monitor.monitor.name]
+#}
+#
+#resource "bigip_ltm_virtual_server" "http" {
+#  name                       = "/Common/terraform_vs_https"
+#  destination                = local.bigip_private
+#  description                = "VirtualServer-test"
+#  port                       = 80
+#  pool                       = bigip_ltm_pool.pool.name
+#  profiles                   = ["/Common/tcp", "/Common/http"]
+#  source_address_translation = "automap"
+#  translate_address          = "enabled"
+#  translate_port             = "enabled"
+#}
+
 
 #resource "bigip_as3" "as3-example1" {
 #  as3_json    = file("as3.json")
--- gcp/bigip/bot-defense/locals.tf
+++ gcp/bigip/bot-defense/locals.tf
@@ -2,4 +2,5 @@ locals {
   bigip_password  = data.tfe_outputs.bigip.values.bigip_password
   app_ip          = data.tfe_outputs.f5-air.values.app_ip
   bigip_ip        = data.tfe_outputs.bigip.values.bigip_public_addresses[0][0.0]
+  bigip_private   = data.tfe_outputs.bigip.values.bigip_private_addresses[0].0
 }
