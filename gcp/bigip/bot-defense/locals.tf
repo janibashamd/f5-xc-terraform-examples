dc4f7cb86962341ab29f51b062cc1adb8982fa28
Janibasha <m.janibasha@f5.com>
2024-10-10 14:58:23 +0530

    corrected tf cloud issue

--- gcp/bigip/bot-defense/locals.tf
+++ gcp/bigip/bot-defense/locals.tf
@@ -1,5 +1,5 @@
 locals {
-  bigip_password  = data.tfe_outputs.bigip.bigip_password
+  bigip_password  = data.tfe_outputs.bigip.values.bigip_password
   app_ip          = data.tfe_outputs.f5-air.values.app_ip
-  bigip_ip        = data.tfe_outputs.bigip.bigip_public_addresses.0
+  bigip_ip        = data.tfe_outputs.bigip.values.bigip_public_addresses.0.0.0
 }
